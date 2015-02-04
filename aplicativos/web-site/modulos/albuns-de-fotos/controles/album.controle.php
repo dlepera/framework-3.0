8359e7484135820a149f62d4e931eb90389a1fa8
dlepera <dlepera@192.168.0.13>
2015-02-04 12:31:21 -0200

    - Correção de BUG nos Álbuns de Fotos (web-site)

--- aplicativos/web-site/modulos/albuns-de-fotos/controles/album.controle.php
+++ aplicativos/web-site/modulos/albuns-de-fotos/controles/album.controle.php
@@ -21,7 +21,7 @@ class Album extends \Geral\Controle\WebSite{
      * -------------------------------------------------------------------------
      */
     public function _mostrarlista(){
-        $this->_listapadrao('album_id, album_nome, log_registro_data_criacao', 'log_registro_data_criacao DESC', 20,
+        $this->_listapadrao('album_id, album_nome, log_registro_data_criacao, foto_album_imagem', 'log_registro_data_criacao DESC', 20,
                 '_listar', false);
 
         # Visão
