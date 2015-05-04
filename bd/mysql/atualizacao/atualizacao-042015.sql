c17c36e894f7564b26a6c806148d93d36de33821
Diego Lepera <dlepera88@gmail.com>
2015-05-04 11:41:36 -0300

    - Ajustes dos arquivos SQL

--- bd/mysql/atualizacao/atualizacao-042015.sql
+++ bd/mysql/atualizacao/atualizacao-042015.sql
@@ -4,6 +4,9 @@ ALTER TABLE dl_painel_usuarios ADD usuario_pref_exibir_id BIT NOT NULL DEFAULT 1
 -- Incluir o campo de preferencia Filtro Menu
 ALTER TABLE dl_painel_usuarios ADD usuario_pref_filtro_menu BIT NOT NULL DEFAULT 0 AFTER usuario_pref_exibir_id;
 
+-- Incluir campo para armazenar o último login
+ALTER TABLE dl_painel_usuarios ADD usuario_ultimo_login DATETIME AFTER usuario_perfil_foto;
+
 
 -- Alterações na base de cadastros do Google Analytics
 ALTER TABLE dl_site_google_analytics CHANGE ga_ativar ga_principal BIT NOT NULL DEFAULT 0;
--- bd/mysql/atualizacao/ultimo-login.sql
+++
@@ -1,2 +0,0 @@
--- Incluir campo para armazenar o último login
-ALTER TABLE dl_painel_usuarios ADD usuario_ultimo_login DATETIME AFTER usuario_perfil_foto;
